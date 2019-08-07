530894d5ee2a802b68cd23b4d6a4258d8b434708
Michael Ansel <github@anselcomputers.com>
2019-08-07 19:58:15 +0000

    Add complex antibody behaviors

--- typeclasses/dn8bossfight/cryptochip.py
+++ typeclasses/dn8bossfight/cryptochip.py
@@ -100,8 +100,14 @@ class AntiVirus(Room):
         self.db.antibodies = []
 
     def recover_cryptochip(self, cryptochip):
-        for _ in range(3):
-            antibody = create.create_object(AntiBody, "antibody", self, home=self, report_to=None)
+        antibodyclasses = [
+            AntiBody,
+            XFirstAntiBody,
+            YFirstAntiBody,
+            WeightedRandomAntiBody,
+        ]
+        for typeclass in antibodyclasses:
+            antibody = create.create_object(typeclass, "antibody", self, home=self, report_to=None)
             antibody.db.antivirus = self
             antibody.set_target(cryptochip)
             self.db.antibodies.append(antibody)
@@ -135,8 +141,12 @@ class AntiBody(Object):
     def at_object_arrive(self, obj, source_location):
         reactor.callLater(0.5, self.capture_target, obj)
 
-    def move_randomly(self):
+    def move_callback(self):
         self.timeout = None
+        self.move()
+
+    def move(self):
+        # Base behavior is to just pick a random exit
         randomized_exits = random.sample(self.location.exits, len(self.location.exits))
         exit = next(exit for exit in randomized_exits)
         if exit:
@@ -162,4 +172,173 @@ class AntiBody(Object):
         if hasattr(self, 'timeout') and self.timeout is not None:
             self.timeout.cancel()
         delay = round(random.random()*1+3) # 1-4seconds
-        self.timeout = reactor.callLater(delay, self.move_randomly)
+        self.timeout = reactor.callLater(delay, self.move_callback)
+
+    def target_coordinates(self):
+        # target -> room
+        if self.db.target.location.db.coordinates is not None:
+            return self.db.target.location.db.coordinates
+        # target -> character -> room
+        if self.db.target.location.location is not None and self.db.target.location.location.db.coordinates is not None:
+            return self.db.target.location.location.db.coordinates
+        return None
+
+results = search.objects('ZoneDN8BossFight', typeclass=Zone)
+if len(results) > 0:
+    zone = results[0]
+else:
+    zone = None
+
+def getroom(x,y):
+    for obj in zone.contents:
+        if obj.is_typeclass(Room, exact=False) and obj.db.coordinates is not None and obj.db.coordinates[0] == x and obj.db.coordinates[1] == y:
+            return obj
+    return None
+
+class XFirstAntiBody(AntiBody):
+    def move(self):
+        # debug("antibody moving")
+        target_coordinates = self.target_coordinates()
+        if target_coordinates is None:
+            debug("bad coordinates")
+        target_y, target_x = target_coordinates
+        my_y, my_x = self.location.db.coordinates
+        # debug("target located at (%d,%d)"%(target_y, target_x))
+
+        if target_y < my_y:
+            # debug("go west")
+            new_y = my_y - 1
+            new_x = my_x
+        elif target_y > my_y:
+            # debug("go east")
+            new_y = my_y + 1
+            new_x = my_x
+        elif target_y == my_y:
+            new_y = my_y
+            if target_x < my_x:
+                # debug("go north")
+                new_x = my_x - 1
+            elif target_x > my_x:
+                # debug("go south")
+                new_x = my_x + 1
+            elif target_x == my_x:
+                debug("target found; why aren't we firing?")
+                return
+
+        new_room = getroom(new_y, new_x)
+        debug("antibody moving to "+str(new_room)+" (%d,%d)"%(new_y,new_x))
+        self.move_to(new_room)
+
+class YFirstAntiBody(AntiBody):
+    def move(self):
+        target_coordinates = self.target_coordinates()
+        if target_coordinates is None:
+            debug("bad coordinates")
+        target_x, target_y = target_coordinates
+        my_x, my_y = self.location.db.coordinates
+
+        if target_y < my_y:
+            new_y = my_y - 1
+            new_x = my_x
+        elif target_y > my_y:
+            new_y = my_y + 1
+            new_x = my_x
+        elif target_y == my_y:
+            new_y = my_y
+            if target_x < my_x:
+                new_x = my_x - 1
+            elif target_x > my_x:
+                new_x = my_x + 1
+            elif target_x == my_x:
+                debug("target found; why aren't we firing?")
+                return
+
+        new_room = getroom(new_x, new_y)
+        debug("antibody moving to "+str(new_room)+" (%d,%d)"%(new_x,new_y))
+        self.move_to(new_room)
+
+class WeightedRandomAntiBody(AntiBody):
+    def move(self):
+        target_coordinates = self.target_coordinates()
+        if target_coordinates is None:
+            debug("bad coordinates")
+        target_x, target_y = target_coordinates
+        my_x, my_y = self.location.db.coordinates
+
+        # [0.000, 0.250): towards x
+        # [0.250, 0.500): towards y
+        # [0.500, 0.750): towards x and y
+        # [0.750, 0.875): away x
+        # [0.875, 1.000): away y
+
+        value = random.random()
+        # debug("antibody moving %.3f"% value)
+        if 0.000 <= value and value < 0.250:
+            # move towards x
+            new_y = my_y
+            if target_x < my_x:
+                new_x = my_x - 1
+            elif target_x > my_x:
+                new_x = my_x + 1
+            else:
+                new_x = my_x # results in no movement
+        elif 0.250 <= value and value < 0.500:
+            # move towards y
+            new_x = my_x
+            if target_y < my_y:
+                new_y = my_y - 1
+            elif target_y > my_y:
+                new_y = my_y + 1
+            else:
+                new_y = my_y # results in no movement
+        elif 0.500 <= value and value < 0.750:
+            # move towards x and y
+            if target_x < my_x:
+                new_x = my_x - 1
+            elif target_x > my_x:
+                new_x = my_x + 1
+            else:
+                new_x = my_x # results in no movement
+
+            if target_y < my_y:
+                new_y = my_y - 1
+            elif target_y > my_y:
+                new_y = my_y + 1
+            else:
+                new_y = my_y # results in no movement
+        elif 0.750 <= value and value < 0.875:
+            # move away x
+            new_y = my_y
+            if target_x < my_x:
+                new_x = my_x + 1
+            elif target_x > my_x:
+                new_x = my_x - 1
+            else:
+                if random.random() > 0.5:
+                    new_x = my_x + 1
+                else:
+                    new_x = my_x - 1
+            if new_x < 0:
+                new_x = 0
+            if new_x > 9:
+                new_x = 9
+        elif 0.875 <= value and value < 1.000:
+            # move towards y
+            new_x = my_x
+            if target_y < my_y:
+                new_y = my_y - 1
+            elif target_y > my_y:
+                new_y = my_y + 1
+            else:
+                if random.random() > 0.5:
+                    new_y = my_y + 1
+                else:
+                    new_y = my_y - 1
+            if new_y < 0:
+                new_y = 0
+            if new_y > 10:
+                new_y = 10
+
+        new_room = getroom(new_x, new_y)
+        debug("antibody moving to "+str(new_room)+" (%d,%d)"%(new_x,new_y))
+        self.move_to(new_room)
